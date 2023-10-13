2cf4670fd3240bd660063ad3cca3b48b87461c57
HavocShadow <147788306+HavocShadow@users.noreply.github.com>
2023-10-13 12:35:05 +0700

    Add files via upload

---
+++ app/README.md
@@ -0,0 +1,16 @@
+# Port Scanner
+
+## Deskripsi
+Proyek ini adalah sebuah alat sederhana untuk memindai port pada sebuah host atau jaringan. Alat ini membantu untuk mendeteksi port yang terbuka pada suatu sistem.
+
+## Penggunaan
+1. Pastikan Python telah terinstal.
+2. Jalankan skrip dengan perintah: `python port_scanner.py`.
+3. Masukkan alamat IP atau nama host yang ingin Anda pindai.
+4. Hasil pemindaian akan ditampilkan.
+
+## Kontribusi
+Silakan berkontribusi dengan melakukan *fork* repositori ini, lalu buat *pull request* dengan perubahan yang Anda ajukan.
+
+## Lisensi
+Proyek ini dilisensikan di bawah [MIT License](LICENSE).
---
+++ app/port_scanner.py
@@ -0,0 +1,41 @@
+from scapy.all import *
+from scapy.layers.inet import ICMP, IP, TCP, UDP
+ip = input("[+] Masukkan IP Anda : ")
+target_ip = input("[+] Masukkan IP target : ")
+target_port = int(input("[+] Masukkan port : "))
+protocol_choice = input("[+] Pilih protokol (TCP/UDP/ICMP): ").upper()
+
+if protocol_choice not in ['TCP', 'UDP', 'ICMP']:
+    print('Protokol tidak valid. Pilih antara TCP, UDP, atau ICMP.')
+    exit()
+
+if protocol_choice == 'TCP':
+    response = sr1(IP(dst=target_ip)/TCP(dport=target_port, flags="S"), timeout=1, verbose=0)
+elif protocol_choice == 'UDP':
+    response = sr1(IP(dst=target_ip)/UDP(dport=target_port), timeout=1, verbose=0)
+elif protocol_choice == 'ICMP':
+    response = sr1(IP(dst=target_ip)/ICMP(), timeout=1, verbose=0)
+
+if response:
+    if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+        print(f'[+] Port {target_port} is opened')
+    elif response.haslayer(TCP) and response.getlayer(TCP).flags == 0x14:
+        print(f'[+] Port {target_port} is closed')
+else:
+    print(f'[+] Port Unknown')
+
+# Gunakan IP sebagai protokol default jika tidak ada pilihan lain
+if protocol_choice == 'UDP':
+    packets = IP(src=ip, dst=target_ip)/UDP(dport=target_port)
+elif protocol_choice == 'ICMP':
+    packets = IP(src=ip, dst=target_ip)/ICMP()
+else:  # Default to TCP
+    packets = IP(src=ip, dst=target_ip)/TCP(dport=target_port)
+
+reply = sr1(packets, timeout=1, verbose=False)
+
+if reply:
+    print('[+] Packet successfully sent')
+    reply.show()
+else:
+    print('[+] Packet failed to send')
